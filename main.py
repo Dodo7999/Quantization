0acfcda1fbbb7cee81ead224702fd7cb78dbd5cb
Bratenkov Miron <brat_miron@mail.ru>
2024-06-09 16:56:24 +0700

    commit

--- main.py
+++ main.py
@@ -1,34 +1,96 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, HfArgumentParser, TrainingArguments, \
-    pipeline, logging
-from huggingface_hub import login
+import logging
+
+import numpy as np
 import torch
+from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
+from datasets import load_dataset
+from huggingface_hub import login
+from transformers import AutoTokenizer
 
 login(
     token="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",
-    add_to_git_credential=True
 )
 
 device = torch.device(
     f'cuda:{torch.cuda.current_device()}' if torch.cuda.is_available() else 'cpu')  # the device to load the model onto
 
 model_id = "CohereForAI/aya-23-35B"
-model = AutoModelForCausalLM.from_pretrained(model_id).to(device)
-tokenizer = AutoTokenizer.from_pretrained(
-    model_id
-).to(device)
+quant_path = "aya23_35B_gptq"
 
-# Format message with the command-r-plus chat template
-messages = [{"role": "user", "content": "Anneme onu ne kadar sevdiğimi anlatan bir mektup yaz"}]
-input_ids = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt").to(
-    device)
-## <BOS_TOKEN><|START_OF_TURN_TOKEN|><|USER_TOKEN|>Anneme onu ne kadar sevdiğimi anlatan bir mektup yaz<|END_OF_TURN_TOKEN|><|START_OF_TURN_TOKEN|><|CHATBOT_TOKEN|>
-
-gen_tokens = model.generate(
-    input_ids,
-    max_new_tokens=100,
-    do_sample=True,
-    temperature=0.3,
+quantize_config = BaseQuantizeConfig(
+    bits=4,  # 4 or 8
+    group_size=128,
+    damp_percent=0.01,
+    desc_act=False,  # set to False can significantly speed up inference but the perplexity may slightly bad
+    static_groups=False,
+    sym=True,
+    true_sequential=True,
+    model_name_or_path=None,
+    model_file_base_name="model"
 )
+max_len = 8192
 
-gen_text = tokenizer.decode(gen_tokens[0])
-print(gen_text)
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoGPTQForCausalLM.from_pretrained(model_id, quantize_config, torch_dtype=torch.bfloat16,
+                                            low_cpu_mem_usage=True, )
+
+
+def check_role(x):
+    new = []
+    for i in x:
+        if i['role'] == 'bot':
+            i['role'] = 'assistant'
+        if (i['role'] == 'assistant') or (i['role'] == 'user'):
+            new.append(i)
+    return np.array(new)
+
+
+def chat_template(x):
+    try:
+        s = tokenizer.apply_chat_template(x, tokenize=False, add_generation_prompt=False)
+    except:
+        s = '0'
+    return s
+
+
+def make_chat_aya(x):
+    messages = [{'role': 'user', 'content': x.inputs},
+                {'role': 'assistant', 'content': x.targets}]
+    prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=False)
+
+    return prompt
+
+
+def make_chat_orca(x):
+    messages = [{'role': 'user', 'content': x.question},
+                {'role': 'assistant', 'content': x.response}]
+    prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=False)
+
+    return prompt
+
+
+def load_saiga():
+    data = load_dataset("IlyaGusev/saiga_scored")
+    data = data[data.language.isin(['English', 'Russian'])]
+    data = data[~data['source'].isin(['saiga_bot_gpt_4o', 'saiga'])]
+    data['messages'] = data['messages'].apply(check_role)
+    data['prompts'] = data['messages'].apply(chat_template)
+    data = data[data.prompts != '0'].sample(5000)
+    x = [prompt for prompt in data["prompts"]]
+    res_data = []
+    for text in x:
+        model_inputs = tokenizer([text])
+        input_ids = torch.tensor(model_inputs.input_ids[:max_len], dtype=torch.int)
+        res_data.append(dict(input_ids=input_ids, attention_mask=input_ids.ne(tokenizer.pad_token_id)))
+    return res_data
+
+
+data = load_saiga()
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s [%(name)s] %(message)s", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S"
+)
+model.quantize(data, cache_examples_on_gpu=False, batch_size=1, use_triton=False)
+
+model.save_quantized(quant_path, use_safetensors=True)
+tokenizer.save_pretrained(quant_path)
